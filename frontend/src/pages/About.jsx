aa5d5a656294fb3cfe0541a7b88790e60c3ce178
ARNAVA KUMAR SINHA <87260059+ARNAVSINHA1@users.noreply.github.com>
2023-09-24 09:58:14 +0000

    few changes

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -14,12 +14,12 @@ const About = () => {
         </div>
         <div className="col-md-4">
           <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+          Dak Ghar Niryat Kendra (DNK) is an initiative from the Department of Posts and CBIC. The name translates to "export facilitation center". The service aims to help small and medium business exporters. 
+          DNKs are a one-stop destination for exporters. They provide assistance with export-related documentation, logistics, customs procedures, and packing. 
+          Exporters can electronically file a Portal Bill of Export and then present the parcel at a DNK for export. 122 DNKs have been opened in Post. 
+          The Department of Posts launched the initiative to promote e-commerce through post offices.
+          Dak Ghar Niryat Kendra (DNK) is an initiative of the Department of Posts and Central Board of Indirect Taxes and Customs (CBIC) to provide a one-stop destination for exporters, offering comprehensive assistance and streamlined processes for export-related documentation, logistics, customs procedures, and packing 12. The DNK acts as a single-window counter for MSME exporters, taking care of end-to-end booking, transmission, and delivery of their products all over the world 1. The small exporters are able to electronically file a Portal Bill of Export and then present the parcel at DNK for export 3. As of now, 122 DNKs have been opened in Post Offices 3.
+          You can access the DNK customer portal at dnk.cept.gov.in.
           </p>
         </div>
       </div>
