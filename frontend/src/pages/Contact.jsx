7e7d6b4e10b714a62e141efeaf0a6782edda26c8
Ansh Roshan <75963202+anshace@users.noreply.github.com>
2023-09-24 12:01:50 +0000

    Changed conatc

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,71 +3,200 @@ import Layout from "../components/Layout/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
 const Contact = () => {
   return (
-    <Layout>
-      <h1 className="text-center text-4xl">Contact Us</h1>
-      <div className="row contactus ">
-        
-        <div className="col-md-6 ">
-          <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
-            style={{ width: "90%" }}
-          />
-        </div>
-        <div className="col-md-4 rounded-lg border-2 p-3">
-          
-        <label htmlFor="name" className="text-xl pr-1">First Name: </label>
-        
-        <input placeholder="First Name" type="text" id="name" className=" p-3 m-2 rounded-lg
-                    border-2 border-grey-400 bg-white-100
-                    focus:outline-none" />
-        <br />
-        <label htmlFor="lastName" className="text-xl px-1">Last name: </label>
-        <input
-            placeholder="Last Name"
-            type="text"
-            name="lastName"
-            className=" p-3 m-2 rounded-lg
-                    border-2 border-grey-400 bg-white-100
-                    focus:outline-none"
-            tabIndex="2"
-            id="lastName"
-       />
-       <br />
-       <label htmlFor="email" className="text-xl pl-1 pr-11">Email:   </label>
-<input
-     type="email"
-     name="email"
-     id="email"
-     className="placeholder-gray-400 p-3 m-2 rounded-lg
-     border-2 border-grey-400 bg-white-100
-     focus:outline-none"
-     placeholder="example@gmail.com"
-     tabIndex="3"
-/>
-<br />
-<div className="w-96">
-<label htmlFor="message" className="text-2xl">Message: </label>
-  <div className="relative w-full min-w-[200px]">
-    
-    <textarea
-      id="message"
-      className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
-      placeholder=""
-    ></textarea>
-    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-      Message
-    </label>
-  </div>
-</div>
-
-<br />
-<button className="px-auto bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 rounded border-blue-700 hover:border-blue-500 rounded">
-  Submit
-</button>
-        </div>
+<Layout>
+  {/* <header class="relative w-full border-b bg-white pb-4">
+    <div class="mx-auto flex max-w-7xl items-center justify-between px-4 py-2">
+      <div class="inline-flex items-center space-x-2">
+        <span>
+          <svg
+            width="30"
+            height="30"
+            viewBox="0 0 50 56"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
+              fill="black"
+            ></path>
+          </svg>
+        </span>
+        <span class="font-bold">DevUI</span>
       </div>
-    </Layout>
+      <div class="hidden lg:block">
+        <ul class="inline-flex space-x-8">
+          <li>
+            <a
+              href="#"
+              class="text-sm font-semibold text-gray-800 hover:text-gray-900"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="text-sm font-semibold text-gray-800 hover:text-gray-900"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="text-sm font-semibold text-gray-800 hover:text-gray-900"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="hidden lg:block">
+        <button
+          type="button"
+          class="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+        >
+          Button text
+        </button>
+      </div>
+      <div class="lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="h-6 w-6 cursor-pointer"
+        >
+          <line x1="4" y1="12" x2="20" y2="12"></line>
+          <line x1="4" y1="6" x2="20" y2="6"></line>
+          <line x1="4" y1="18" x2="20" y2="18"></line>
+        </svg>
+      </div>
+    </div>
+  </header> */}
+  <div class="mx-auto max-w-7xl px-4">
+    <div class="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
+      <div class="mx-auto max-w-max rounded-full border bg-gray-50 p-1 px-3">
+        <p class="text-center text-xs font-semibold leading-normal md:text-sm">
+          Share your thoughts
+        </p>
+      </div>
+      <p class="text-center text-3xl font-bold text-gray-900 md:text-5xl md:leading-10">
+        Love to hear from you
+      </p>
+      <p class="mx-auto max-w-4xl text-center text-base text-gray-600 md:text-xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        veritatis voluptates neque itaque repudiandae sint, explicabo assumenda
+        quam ratione placeat?
+      </p>
+    </div>
+    <div class="mx-auto max-w-7xl py-12 md:py-24">
+      <div class="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
+        <div class="flex items-center justify-center">
+          <div class="px-2 md:px-12">
+            <p class="text-2xl font-bold text-gray-900 md:text-4xl">
+              Get in touch
+            </p>
+            <p class="mt-4 text-lg text-gray-600">
+              Our friendly team would love to hear from you.
+            </p>
+            <form action="" class="mt-8 space-y-4">
+              <div class="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
+                <div class="grid w-full  items-center gap-1.5">
+                  <label
+                    class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    for="first_name"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                    type="text"
+                    id="first_name"
+                    placeholder="First Name"
+                  />
+                </div>
+                <div class="grid w-full  items-center gap-1.5">
+                  <label
+                    class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    for="last_name"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                    type="text"
+                    id="last_name"
+                    placeholder="Last Name"
+                  />
+                </div>
+              </div>
+              <div class="grid w-full  items-center gap-1.5">
+                <label
+                  class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  for="email"
+                >
+                  Email
+                </label>
+                <input
+                  class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                  type="text"
+                  id="email"
+                  placeholder="Email"
+                />
+              </div>
+              <div class="grid w-full  items-center gap-1.5">
+                <label
+                  class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  for="phone_number"
+                >
+                  Phone number
+                </label>
+                <input
+                  class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                  type="tel"
+                  id="phone_number"
+                  placeholder="Phone number"
+                />
+              </div>
+              <div class="grid w-full  items-center gap-1.5">
+                <label
+                  class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  for="message"
+                >
+                  Message
+                </label>
+                <textarea
+                  class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                  id="message"
+                  placeholder="Leave us a message"
+                  cols="3"
+                ></textarea>
+              </div>
+              <button
+                type="button"
+                class="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
+        <img
+          alt="Contact us"
+          class="hidden max-h-full w-full rounded-lg object-cover lg:block"
+          src="https://images.unsplash.com/photo-1615840287214-7ff58936c4cf?ixlib=rb-4.0.3&amp;auto=format&amp;fit=crop&amp;w=687&amp;h=800&amp;q=80"
+        />
+      </div>
+    </div>
+  </div>
+</Layout>
+
   );
 };
 
