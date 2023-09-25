a59d5f3ad9a8343eaa1eba789978a62b40e7479f
Sumanjeet <sumanjeet0012@gmail.com>
2023-09-25 07:21:12 +0000

    corrected message text area in contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -96,12 +96,12 @@ const Contact = () => {
                     >
                       Message
                     </label>
-                    <textarea
-                      class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50    "
-                      id="message"
+                    <textarea 
+                      class="flex h-40 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      id="message" 
                       placeholder="Leave us a message"
-                      cols="3"
-                    ></textarea>
+                      cols="3">
+                    </textarea>
                   </div>
                   <button
                     type="button"
