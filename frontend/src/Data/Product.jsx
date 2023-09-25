44597a9a5e91e0fc252ce088cb35a51e999a51c8
Sumanjeet <sumanjeet0012@gmail.com>
2023-09-25 12:55:52 +0000

    added some products in product.jsx

--- frontend/src/Data/Product.jsx
+++ frontend/src/Data/Product.jsx
@@ -1,42 +1,58 @@
 export const Product = [
   {
-    id: 1,
-    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    price: 109.95,
-    rating: 3.9,
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    link: "https://fakestoreapi.com/products/fjallraven-foldsack-no-1-backpack",
+    id: 1, 
+    name: 'Handmade Cotton Quilt', 
+    description: 'Vintage style handmade quilt with traditional patterns and colors',
+    price: 89.99, 
+    rating: 4.5,
+    image: 'https://th.bing.com/th?id=OPA.9sccm7IctLvBJQ474C474&w=592&h=550&o=5&pid=21.1'
   },
   {
     id: 2,
-    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    price: 109.95,
-    rating: 3.9,
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    link: "https://fakestoreapi.com/products/fjallraven-foldsack-no-1-backpack",
+    name: 'Leather Wallet for Men',
+    description: 'Handcrafted leather wallet, multiple card slots, durable and stylish',
+    price: 29.99,
+    rating: 4.7,
+    image: 'https://th.bing.com/th?id=OPA.clPY3JqcAGAkIw474C474&w=592&h=550&o=5&pid=21.1'
   },
   {
     id: 3,
-    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    price: 109.95,
-    rating: 3.9,
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    link: "https://fakestoreapi.com/products/fjallraven-foldsack-no-1-backpack",
+    name: 'Silver Gemstone Pendant',
+    description: 'Handmade sterling silver pendant with inlaid natural gemstones',
+    price: 89.99,
+    rating: 4.8,
+    image: 'https://www.giva.co/cdn/shop/products/PD01570_1-min_1024x1024.jpg?v=1676563673' 
   },
   {
     id: 4,
-    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    price: 109.95,
-    rating: 3.9,
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    link: "https://fakestoreapi.com/products/fjallraven-foldsack-no-1-backpack",
+    name: 'Leather Casual Shoes for Men',
+    description: 'Handcrafted leather upper, rubber sole, cushioned footbed', 
+    price: 69.99,
+    rating: 4.5,
+    image: 'https://ae01.alicdn.com/kf/H25a36e5dfdb243bf93a08b34bd3f41376/Men-Business-Casual-Shoes-PU-Leather-Running-Shoes-Fashion-Lace-Up-Casual-Sneakers-Male-Outdoor-Walking.jpg'
   },
-]
+  {  
+    id: 5,
+    name: 'Stainless Steel Water Bottle',
+    description: 'Vacuum insulated, double walled, leak proof, keeps drinks hot/cold',
+    price: 19.99,
+    rating: 4.6,
+    image: 'https://th.bing.com/th?id=OPA.QBKxYkXwfPNX6w474C474&w=592&h=550&o=5&pid=21.1'
+  },
+  {
+    id: 6,
+    name: 'Organic Basmati Rice', 
+    description: 'Premium aged basmati rice, 100% organic, aromatic long grains',
+    price: 11.99,
+    rating: 4.8,
+    image: 'https://th.bing.com/th?id=OPA.WJseZ6oF8jeU2Q474C474&w=592&h=550&o=5&pid=21.1'
+  },
+  {
+    id: 7,
+    name: 'Silver Plated Jewellery Set',
+    description: 'Intricate handcrafted silver plated necklace set for women',
+    price: 49.99,
+    rating: 4.6, 
+    image: 'https://th.bing.com/th?id=OPA.dgdMFCQdWFvI6Q474C474&w=592&h=550&o=5&pid=21.1'
+  },
+];
