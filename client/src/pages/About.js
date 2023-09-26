e2cd7151c5517bc3c5fbed85910bdb13188639be
Ayushi Priya <ayushipriya@example.com>
2023-09-26 23:30:43 +0530

    ayushi 2nd commit

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,13 +4,14 @@ import Layout from "./../components/Layout/Layout";
 const About = () => {
   return (
     <Layout>
+      
       <div className="row contactus ">
-        <div className="col-md-6 ">
           <img
             src="/images/about.jpeg"
             alt="contactus"
             style={{ width: "100%" }}
           />
+        <div className="col-md-6 ">
         </div>
         <div className="col-md-4">
           <p className="text-justify mt-2">
