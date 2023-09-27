ea25735f53aa1e8842037ae2e2585268c5d2ec3a
Ayushi Priya <90374474+ayushi825@users.noreply.github.com>
2023-09-27 12:41:08 +0530

    Update AuthHeader.jsx

--- frontend/src/components/Layout/AuthHeader.jsx
+++ frontend/src/components/Layout/AuthHeader.jsx
@@ -13,7 +13,7 @@ function AuthHeader() {
       <nav class='flex justify-between  bg-sky-950 text-white w-screen'>
         <div class='px-5 xl:px-12 py-6 flex w-full items-center'>
           <a class='text-3xl font-bold font-heading' href='#'>
-            <img src='/logo.png' alt='image' width='54' height='54' />
+            <img src='/logo.png' alt='image' width='54' height='54' />DNK
           </a>
           <ul class='hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12'>
             <li>
